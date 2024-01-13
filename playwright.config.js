c70b0cc22f47230fe404ef58cd638084451a346c
Ruslan Vasiliev <it.vasiliev@gmail.com>
2024-01-13 14:34:26 +0200

    hw-14

--- playwright.config.js
+++ playwright.config.js
@@ -26,7 +26,6 @@ module.exports = defineConfig({
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: `https://${process.env.AUTHENTICATION_NAME}:${process.env.AUTHENTICATION_PASSWORD}@qauto.forstudy.space`,
-   // baseURL: `https://guest:welcome2qauto@qauto.forstudy.space/`,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
 
