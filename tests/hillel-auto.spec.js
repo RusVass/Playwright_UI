7c8d481dd26d722294891f97b5a93e2385e18e04
Ruslan Vasiliev <it.vasiliev@gmail.com>
2024-01-14 13:09:42 +0200

    commit hw-14

--- tests/hillel-auto.spec.js
+++ tests/hillel-auto.spec.js
@@ -35,6 +35,7 @@ test.describe('hillel auto', () =>{
 
         await test.step("click on Register button", async () => {
         await page.locator('Button', {hasText: "Register"}).click()
+        await expect(page.locator('div h1')).toBeVisible()
         })
     })
 })
